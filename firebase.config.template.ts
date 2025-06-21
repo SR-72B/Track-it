380ce3bf06c9e808c5ee1d268fa9286645ed5058
SR-72B <shah43945@gmail>
2025-06-21 17:42:07 -0400

    Secure Firebase configuration - add templates and ignore sensitive files

--- firebase.config.template.ts
+++ firebase.config.template.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyATHHTL8TqoLdRXhNzZcQzEWOoHmef4MSo",
+  apiKey: "YOUR_API_KEY_HERE",
   authDomain: "track-it-30b38.firebaseapp.com",
   databaseURL: "https://track-it-30b38-default-rtdb.firebaseio.com",
   projectId: "track-it-30b38",
   storageBucket: "track-it-30b38.firebasestorage.app",
-  messagingSenderId: "566924400410",
-  appId: "1:566924400410:web:70f359f697c9001b24fea3",
-  measurementId: "G-CFHN2CLLSJ"
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID_HERE",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
---
+++ src/environments/environment.template.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'YOUR_API_KEY_HERE',
+    authDomain: 'track-it-30b38.firebaseapp.com',
+    databaseURL: 'https://track-it-30b38-default-rtdb.firebaseio.com',
+    projectId: 'track-it-30b38',
+    storageBucket: 'track-it-30b38.firebasestorage.app',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID_HERE',
+    measurementId: 'YOUR_MEASUREMENT_ID'
+  }
+};
